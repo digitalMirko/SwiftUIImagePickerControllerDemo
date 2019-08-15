85c6f540828a7beb1655727699c2e82a9c3a17da
Mirko Cukich <mirkocukich@gmail.com>
2019-08-14 20:39:00 -0500

    Resources and Constraints placed on storyboards

--- UIImage Picker Cont/ViewController.swift
+++ UIImage Picker Cont/ViewController.swift
@@ -10,12 +10,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    
+    @IBAction func cameraBtn(_ sender: Any) {
+        
+    }
+    
 }
 
